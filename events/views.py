ca2ff729a7c22210a3f22f0a4ca987b819f23764
Rohith04MVK <007rohitjj04@gmail.com>
2023-04-10 06:19:29 +0530

    Added a an  option to edit the events

--- events/views.py
+++ events/views.py
@@ -1,14 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render,  get_object_or_404
 from django.urls import reverse_lazy
 
 from .models import Event
+from .forms import EventForm
+
 
 @login_required
 def event_list(request):
     events = Event.objects.all()
     return render(request, 'events/event_list.html', {'events': events})
+
 
 @login_required(login_url="/login")
 def add_event(request):
@@ -19,3 +22,15 @@ def add_event(request):
         Event.objects.create(image=image, title=title, description=description)
         return redirect('event_list')
     return render(request, 'events/add_event.html')
+
+
+def edit_event(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    if request.method == 'POST':
+        form = EventForm(request.POST, instance=event)
+        if form.is_valid():
+            form.save()
+            return redirect('all_events')
+    else:
+        form = EventForm(instance=event)
+    return render(request, 'edit_event.html', {'form': form, 'event_id': event_id})
