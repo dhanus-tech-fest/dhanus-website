7580858939e0c7c21631a19d79596fcf96abfaa8
Rohith04MVK <007rohitjj04@gmail.com>
2023-04-15 09:49:52 +0530

    Added a an  option to edit the events

--- events/models.py
+++ events/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Event(models.Model):
     title = models.CharField(max_length=255)
     is_active = models.CharField(max_length=10)
+    contact = models.CharField(max_length=255)
     description = models.TextField()
     prize = models.TextField()
     guidelines = models.TextField()
--- events/views.py
+++ events/views.py
@@ -23,8 +23,9 @@ def add_event(request):
         guidelines = request.POST["guidelines"]
         is_active = request.POST["is_active"]
         konf_hub_url = request.POST["konf_hub_url"]
+        contact = request.POST["contact"]
         Event.objects.create(image=image, title=title, description=description, prize=prize,
-                             konf_hub_url=konf_hub_url, is_active=is_active, guidelines=guidelines)
+                             konf_hub_url=konf_hub_url, is_active=is_active, guidelines=guidelines, contact=contact)
         return redirect('event_list')
     return render(request, 'events/add_event.html')
 
